589bcfd998da7b6997fa70c87215157d84234953
Junkai Huang <absphras2967751839@gmail.com>
2024-12-02 16:59:03 +0800

    创建项目

---
+++ src/main/java/com/junkai/picture_enhancement_platform/PictureEnhancementPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.junkai.picture_enhancement_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureEnhancementPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PictureEnhancementPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/junkai/picture_enhancement_platform/PictureEnhancementPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junkai.picture_enhancement_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PictureEnhancementPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
